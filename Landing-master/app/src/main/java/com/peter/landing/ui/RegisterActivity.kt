cb2909faaffed3a5c7c8d4a83d7f65196f0b9a66
zy <3219920162@qq.com>
2025-05-10 19:10:28 +0800

    git代码更新出问题，尝试提交

--- Landing-master/app/src/main/java/com/peter/landing/ui/RegisterActivity.kt
+++ Landing-master/app/src/main/java/com/peter/landing/ui/RegisterActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.peter.landing.R
-import com.peter.landing.data.UserDatabase
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
